003a184981422ab04a942c71c9802f01ab715001
jwfing <jwfing@gmail.com>
2018-03-22 18:42:09 +0800

    adjust android sdk build env

--- android-sdk/src/test/java/cn/leancloud/Configure.java
+++
@@ -1,6 +0,0 @@
-package cn.leancloud;
-
-public class Configure {
-  public static final String TEST_APP_ID = "ohqhxu3mgoj2eyj6ed02yliytmbes3mwhha8ylnc215h0bgk";
-  public static final String TEST_APP_KEY = "6j8fuggqkbc5m86b8mp4pf2no170i5m7vmax5iypmi72wldc";
-}
