6f276eee6a80df3cc433183991347bd8fb06b622
jwfing <jwfing@gmail.com>
2018-08-06 17:44:22 +0800

    add more testcase for realtime

--- realtime/src/test/java/cn/leancloud/im/AVIMConversationTest.java
+++ realtime/src/test/java/cn/leancloud/im/AVIMConversationTest.java
@@ -1,29 +1,27 @@
 package cn.leancloud.im;
 
 import cn.leancloud.Configure;
-import cn.leancloud.im.v2.AVIMClient;
-import cn.leancloud.im.v2.AVIMConversation;
-import cn.leancloud.im.v2.AVIMException;
-import cn.leancloud.im.v2.AVIMMessageManager;
-import cn.leancloud.im.v2.callback.AVIMClientCallback;
-import cn.leancloud.im.v2.callback.AVIMConversationCallback;
-import cn.leancloud.im.v2.callback.AVIMConversationCreatedCallback;
+import cn.leancloud.im.v2.*;
+import cn.leancloud.im.v2.callback.*;
 import cn.leancloud.im.v2.messages.AVIMTextMessage;
 import cn.leancloud.session.AVConnectionManager;
 import junit.framework.TestCase;
 
+import java.sql.Time;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class AVIMConversationTest extends TestCase {
   private CountDownLatch countDownLatch = null;
   private boolean opersationSucceed = false;
-  private AVIMClient client = AVIMClient.getInstance("testUser1");
+  private AVIMClient client = null;
   private List<String> memebers = Arrays.asList("User2", "User3");
   private String convName = "RealtimeUnitTest";
+  private DummyConversationEventHandler conversationEventHandler = new DummyConversationEventHandler(0x00FFFF);
 
   public AVIMConversationTest(String suiteName) {
     super(suiteName);
@@ -35,14 +33,35 @@ public class AVIMConversationTest extends TestCase {
       ex.printStackTrace();
     }
     AVIMClient.setClientEventHandler(new DummyClientEventHandler());
-    AVIMMessageManager.setConversationEventHandler(new DummyConversationEventHandler());
+    AVIMMessageManager.setConversationEventHandler(conversationEventHandler);
   }
 
   @Override
   protected void setUp() throws Exception {
     this.countDownLatch = new CountDownLatch(1);
     opersationSucceed = false;
+    conversationEventHandler.resetAllCount();
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    this.countDownLatch = null;
     CountDownLatch tmpCounter = new CountDownLatch(1);
+    if (null != client) {
+      client.close(new AVIMClientCallback() {
+        @Override
+        public void done(AVIMClient client, AVIMException e) {
+          tmpCounter.countDown();
+        }
+      });
+      tmpCounter.await();
+    }
+    client = null;
+  }
+
+  public void testSendTextMessage() throws Exception {
+    CountDownLatch tmpCounter = new CountDownLatch(1);
+    client = AVIMClient.getInstance("testUser1");
     client.open(new AVIMClientCallback() {
       @Override
       public void done(AVIMClient client, AVIMException e) {
@@ -50,22 +69,6 @@ public class AVIMConversationTest extends TestCase {
       }
     });
     tmpCounter.await();
-  }
-
-  @Override
-  protected void tearDown() throws Exception {
-    this.countDownLatch = null;
-    CountDownLatch tmpCounter = new CountDownLatch(1);
-    client.close(new AVIMClientCallback() {
-      @Override
-      public void done(AVIMClient client, AVIMException e) {
-        tmpCounter.countDown();
-      }
-    });
-    tmpCounter.await();
-  }
-
-  public void testSendTextMessage() throws Exception {
     client.createConversation(memebers, convName, null, false, true, new AVIMConversationCreatedCallback() {
       @Override
       public void done(AVIMConversation conversation, AVIMException e) {
@@ -96,6 +99,15 @@ public class AVIMConversationTest extends TestCase {
   }
 
   public void testJoinedNotification() throws Exception {
+    client = AVIMClient.getInstance("testUser1");
+    CountDownLatch tmpCounter = new CountDownLatch(1);
+    client.open(new AVIMClientCallback() {
+      @Override
+      public void done(AVIMClient client, AVIMException e) {
+        tmpCounter.countDown();
+      }
+    });
+    tmpCounter.await();
     Map<String, Object> attr = new HashMap<>();
     attr.put("testTs", System.currentTimeMillis());
     attr.put("owner", "testUser1");
@@ -127,5 +139,152 @@ public class AVIMConversationTest extends TestCase {
     countDownLatch.await();
     assertTrue(opersationSucceed);
     Thread.sleep(2000);
+  }
+
+  public void testDummyConversationEventHandlerCounter() throws Exception {
+    AVIMClient client = AVIMClient.getInstance("testUser");
+    AVIMConversation conv = client.getConversation("conversationId");
+    this.conversationEventHandler.onBlocked(client, conv, "nobody");
+    this.conversationEventHandler.onInvited(client, conv, "nobody");
+    this.conversationEventHandler.onMemberBlocked(client, conv, Arrays.asList("Tom", "Jerry"),"nobody");
+    assertTrue(this.conversationEventHandler.getCount(0x00ffFF) == 3);
+  }
+
+  public void testSendAndReceiveMessage() throws Exception {
+    final String senderId = "sender-" + System.currentTimeMillis();
+    Thread.sleep(8000);
+    final String receiverId = "receiver-" + System.currentTimeMillis();
+
+    CountDownLatch receierOnlineLatch = new CountDownLatch(1);
+
+    Runnable sendThread = new Runnable() {
+      @Override
+      public void run() {
+        CountDownLatch tmpCounter = new CountDownLatch(1);
+        AVIMClient client = AVIMClient.getInstance(senderId);
+        client.open(new AVIMClientCallback() {
+          @Override
+          public void done(AVIMClient client, AVIMException e) {
+            if (null != e) {
+              System.out.println("failed to open sender client.");
+              e.printStackTrace();
+              tmpCounter.countDown();
+            } else {
+              try {
+                receierOnlineLatch.await();
+              } catch (Exception ex) {
+                ex.printStackTrace();
+              }
+              client.createConversation(Arrays.asList(receiverId), null, null, false, true,
+                      new AVIMConversationCreatedCallback() {
+                        @Override
+                        public void done(AVIMConversation conversation, AVIMException e) {
+                          if (null != e) {
+                            System.out.println("failed to create conversation from sender client.");
+                            e.printStackTrace();
+                            tmpCounter.countDown();
+                          } else {
+                            conversation.blockMembers(Arrays.asList("blockedUser"), new AVIMOperationPartiallySucceededCallback() {
+                              @Override
+                              public void done(AVIMException e, List<String> successfulClientIds, List<AVIMOperationFailure> failures) {
+                                if (null != e) {
+                                  System.out.println("failed to block members from sender client.");
+                                  tmpCounter.countDown();
+                                } else {
+                                  AVIMTextMessage msg = new AVIMTextMessage();
+                                  msg.setText("try to unblock user. @" + System.currentTimeMillis());
+                                  conversation.sendMessage(msg, new AVIMConversationCallback() {
+                                    @Override
+                                    public void done(AVIMException e) {
+                                      if (null != e) {
+                                        System.out.println("failed to send message from sender client.");
+                                        tmpCounter.countDown();
+                                      } else {
+                                        conversation.unblockMembers(Arrays.asList("blockedUser"), new AVIMOperationPartiallySucceededCallback() {
+                                          @Override
+                                          public void done(AVIMException e, List<String> successfulClientIds, List<AVIMOperationFailure> failures) {
+                                            if (null != e) {
+                                              System.out.println("failed to unblock members from sender client.");
+                                              e.printStackTrace();
+                                            }
+                                            client.close(new AVIMClientCallback() {
+                                              @Override
+                                              public void done(AVIMClient client, AVIMException e) {
+                                                if (null != e) {
+                                                  System.out.println("failed to close sender client.");
+                                                } else {
+                                                  System.out.println("succeed to run all flow on sender side.");
+                                                }
+                                                tmpCounter.countDown();
+                                              }
+                                            });
+                                          }
+                                        });
+                                      }
+                                    }
+                                  });
+
+                                }
+                              }
+                            });
+                          }
+                        }
+                      });
+            }
+          }
+        });
+        try {
+          tmpCounter.await(60, TimeUnit.SECONDS);
+        } catch (Exception ex) {
+          ex.printStackTrace();
+        }
+      }
+    };
+    Runnable receiveThread = new Runnable() {
+      @Override
+      public void run() {
+        CountDownLatch tmpCounter = new CountDownLatch(1);
+        AVIMClient client = AVIMClient.getInstance(receiverId);
+        client.open(new AVIMClientCallback() {
+          @Override
+          public void done(AVIMClient client, AVIMException e) {
+            receierOnlineLatch.countDown();
+            if (null != e) {
+              System.out.println("failed to open receiver client");
+              e.printStackTrace();
+              tmpCounter.countDown();
+            } else {
+              try {
+                Thread.sleep(60000);
+              } catch (Exception ex) {
+                ex.printStackTrace();
+              }
+              client.close(new AVIMClientCallback() {
+                @Override
+                public void done(AVIMClient client, AVIMException e) {
+                  if (null != e) {
+                    System.out.println("failed to close receiver client");
+                    e.printStackTrace();
+                  }
+                  tmpCounter.countDown();
+                }
+              });
+            }
+          }
+        });
+        try {
+          tmpCounter.await(60, TimeUnit.SECONDS);
+        } catch (Exception ex) {
+          ex.printStackTrace();
+        }
+      }
+    };
+    new Thread(sendThread).start();
+    Thread.sleep(6000);
+    new Thread(receiveThread).start();
+    Thread.sleep(80000);
+    int notifyCount = this.conversationEventHandler.getCount(0x00FFFF);
+    System.out.println("notifyCount=" + notifyCount);
+    assertTrue(notifyCount > 2);
   }
 }
--- realtime/src/test/java/cn/leancloud/im/DummyConversationEventHandler.java
+++ realtime/src/test/java/cn/leancloud/im/DummyConversationEventHandler.java
@@ -9,32 +9,111 @@ import cn.leancloud.utils.StringUtil;
 import java.util.List;
 
 public class DummyConversationEventHandler extends AVIMConversationEventHandler {
+  static final int NOTIFY_INDEX_INVITED = 1;
+  static final int NOTIFY_INDEX_KICKED = 2;
+  static final int NOTIFY_INDEX_MUTED = 3;
+  static final int NOTIFY_INDEX_UNMUTED = 4;
+  static final int NOTIFY_INDEX_BLOCKED = 5;
+  static final int NOTIFY_INDEX_UNBLOCKED = 6;
+  static final int NOTIFY_INDEX_MEMBERLEFT = 7;
+  static final int NOTIFY_INDEX_MEMBERJOINED = 8;
+  static final int NOTIFY_INDEX_MEMBERMUTED = 9;
+  static final int NOTIFY_INDEX_MEMBERUNMUTED = 10;
+  static final int NOTIFY_INDEX_MEMBERBLOCKED = 11;
+  static final int NOTIFY_INDEX_MEMBERUNBLOCKED = 12;
+  static final int NOTIFY_INDEX_MSGUPDATED = 13;
+  static final int NOTIFY_INDEX_MSGRECALLED = 14;
+
+  static final int NOTIFY_COUNT = 15;
+
+  static final int FLAG_NOTIFY_INVITED = 0x0001 << (NOTIFY_INDEX_INVITED - 1);
+  static final int FLAG_NOTIFY_KICKED = 0x0001 << (NOTIFY_INDEX_KICKED - 1);
+  static final int FLAG_NOTIFY_MUTED = 0x0001 << (NOTIFY_INDEX_MUTED - 1);
+  static final int FLAG_NOTIFY_UNMUTED = 0x0001 << (NOTIFY_INDEX_UNMUTED - 1);
+  static final int FLAG_NOTIFY_BLOCKED = 0x0001 << (NOTIFY_INDEX_BLOCKED - 1);
+  static final int FLAG_NOTIFY_UNBLOCKED = 0x0001 << (NOTIFY_INDEX_UNBLOCKED - 1);
+  static final int FLAG_NOTIFY_MEMBERLEFT = 0x0001 << (NOTIFY_INDEX_MEMBERLEFT - 1);
+  static final int FLAG_NOTIFY_MEMBERJOINED = 0x0001 << (NOTIFY_INDEX_MEMBERJOINED - 1);
+  static final int FLAG_NOTIFY_MEMBERMUTED = 0x0001 << (NOTIFY_INDEX_MEMBERMUTED - 1);
+  static final int FLAG_NOTIFY_MEMBERUNMUTED = 0x0001 << (NOTIFY_INDEX_MEMBERUNMUTED - 1);
+  static final int FLAG_NOTIFY_MEMBERBLOCKED = 0x0001 << (NOTIFY_INDEX_MEMBERBLOCKED - 1);
+  static final int FLAG_NOTIFY_MEMBERUNBLOCKED = 0x0001 << (NOTIFY_INDEX_MEMBERUNBLOCKED - 1);
+  static final int FLAG_NOTIFY_MSGUPDATED = 0x0001 << (NOTIFY_INDEX_MSGUPDATED - 1);
+  static final int FLAG_NOTIFY_MSGRECALLED = 0x0001 << (NOTIFY_INDEX_MSGRECALLED - 1);
+
+  private int notifyConfig = 0;
+  private int[] count = new int[NOTIFY_COUNT];
+
+  public DummyConversationEventHandler() {
+    this(0);
+  }
+  public DummyConversationEventHandler(int notifyConfig) {
+    this.notifyConfig = notifyConfig;
+    resetAllCount();
+  }
+
+  public void resetAllCount() {
+    for (int i = 0; i< NOTIFY_COUNT; i++) {
+      count[i] = 0;
+    }
+  }
+
+  public int getCount(int config) {
+    int result = 0;
+    for (int i = 0; i < NOTIFY_COUNT; i++) {
+      int flag = (0x0001 << i);
+      if ((config & flag) == flag) {
+        result += count[i];
+      }
+    }
+
+    return result;
+  }
+
   public void onMemberLeft(AVIMClient client,
                            AVIMConversation conversation, List<String> members, String kickedBy) {
     LOGGER.d("Notification --- onMemberLeft. client=" + client.getClientId() + ", convId=" + conversation.getConversationId() + ", by=" + kickedBy );
+    if ((notifyConfig & FLAG_NOTIFY_MEMBERLEFT) == FLAG_NOTIFY_MEMBERLEFT) {
+      count[NOTIFY_INDEX_MEMBERLEFT] ++;
+    }
   }
 
   public void onMemberJoined(AVIMClient client,
                                       AVIMConversation conversation, List<String> members, String invitedBy) {
     LOGGER.d("Notification --- onMemberJoined. client=" + client.getClientId() + ", convId=" + conversation.getConversationId() + ", by=" + invitedBy );
+    if ((notifyConfig & FLAG_NOTIFY_MEMBERJOINED) == FLAG_NOTIFY_MEMBERJOINED) {
+      count[NOTIFY_INDEX_MEMBERJOINED] ++;
+    }
   }
 
   public void onKicked(AVIMClient client, AVIMConversation conversation,
                                 String kickedBy) {
     LOGGER.d("Notification --- onKicked. client=" + client.getClientId() + ", convId=" + conversation.getConversationId() + ", by=" + kickedBy );
+    if ((notifyConfig & FLAG_NOTIFY_KICKED) == FLAG_NOTIFY_KICKED) {
+      count[NOTIFY_INDEX_KICKED] ++;
+    }
   }
 
   public void onInvited(AVIMClient client, AVIMConversation conversation,
                                  String operator) {
     LOGGER.d("Notification --- onInvited. client=" + client.getClientId() + ", convId=" + conversation.getConversationId() + ", by=" + operator );
+    if ((notifyConfig & FLAG_NOTIFY_INVITED) == FLAG_NOTIFY_INVITED) {
+      count[NOTIFY_INDEX_INVITED] ++;
+    }
   }
 
   public void onMuted(AVIMClient client, AVIMConversation conversation, String operator) {
     LOGGER.d("Notification --- " + " you are muted by " + operator );
+    if ((notifyConfig & FLAG_NOTIFY_MUTED) == FLAG_NOTIFY_MUTED) {
+      count[NOTIFY_INDEX_MUTED] ++;
+    }
   }
 
   public void onUnmuted(AVIMClient client, AVIMConversation conversation, String operator) {
     LOGGER.d("Notification --- " + " you are unmuted by " + operator );
+    if ((notifyConfig & FLAG_NOTIFY_UNMUTED) == FLAG_NOTIFY_UNMUTED) {
+      count[NOTIFY_INDEX_UNMUTED] ++;
+    }
   }
 
   /**
@@ -46,6 +125,9 @@ public class DummyConversationEventHandler extends AVIMConversationEventHandler 
    */
   public void onMemberMuted(AVIMClient client, AVIMConversation conversation, List<String> members, String operator){
     LOGGER.d("Notification --- " + operator + " muted members: " + StringUtil.join(", ", members));
+    if ((notifyConfig & FLAG_NOTIFY_MEMBERMUTED) == FLAG_NOTIFY_MEMBERMUTED) {
+      count[NOTIFY_INDEX_MEMBERMUTED] ++;
+    }
   }
 
   /**
@@ -57,6 +139,9 @@ public class DummyConversationEventHandler extends AVIMConversationEventHandler 
    */
   public void onMemberUnmuted(AVIMClient client, AVIMConversation conversation, List<String> members, String operator){
     LOGGER.d("Notification --- " + operator + " unmuted members: " + StringUtil.join(", ", members));
+    if ((notifyConfig & FLAG_NOTIFY_MEMBERUNMUTED) == FLAG_NOTIFY_MEMBERUNMUTED) {
+      count[NOTIFY_INDEX_MEMBERUNMUTED] ++;
+    }
   }
 
   /**
@@ -67,6 +152,9 @@ public class DummyConversationEventHandler extends AVIMConversationEventHandler 
    */
   public void onBlocked(AVIMClient client, AVIMConversation conversation, String operator) {
     LOGGER.d("Notification --- " + " you are blocked by " + operator );
+    if ((notifyConfig & FLAG_NOTIFY_BLOCKED) == FLAG_NOTIFY_BLOCKED) {
+      count[NOTIFY_INDEX_BLOCKED] ++;
+    }
   }
 
   /**
@@ -77,6 +165,9 @@ public class DummyConversationEventHandler extends AVIMConversationEventHandler 
    */
   public void onUnblocked(AVIMClient client, AVIMConversation conversation, String operator) {
     LOGGER.d("Notification --- " + " you are unblocked by " + operator );
+    if ((notifyConfig & FLAG_NOTIFY_UNBLOCKED) == FLAG_NOTIFY_UNBLOCKED) {
+      count[NOTIFY_INDEX_UNBLOCKED] ++;
+    }
   }
 
   /**
@@ -88,6 +179,9 @@ public class DummyConversationEventHandler extends AVIMConversationEventHandler 
    */
   public void onMemberBlocked(AVIMClient client, AVIMConversation conversation, List<String> members, String operator){
     LOGGER.d("Notification --- " + operator + " blocked members: " + StringUtil.join(", ", members));
+    if ((notifyConfig & FLAG_NOTIFY_MEMBERBLOCKED) == FLAG_NOTIFY_MEMBERBLOCKED) {
+      count[NOTIFY_INDEX_MEMBERBLOCKED] ++;
+    }
   }
 
   /**
@@ -99,6 +193,9 @@ public class DummyConversationEventHandler extends AVIMConversationEventHandler 
    */
   public void onMemberUnblocked(AVIMClient client, AVIMConversation conversation, List<String> members, String operator){
     LOGGER.d("Notification --- " + operator + " unblocked members: " + StringUtil.join(", ", members));
+    if ((notifyConfig & FLAG_NOTIFY_MEMBERUNBLOCKED) == FLAG_NOTIFY_MEMBERUNBLOCKED) {
+      count[NOTIFY_INDEX_MEMBERUNBLOCKED] ++;
+    }
   }
 
   /**
@@ -132,6 +229,9 @@ public class DummyConversationEventHandler extends AVIMConversationEventHandler 
    */
   public void onMessageUpdated(AVIMClient client, AVIMConversation conversation, AVIMMessage message) {
     LOGGER.d("Notification --- onMessageUpdated. client=" + client.getClientId() + ", convId=" + conversation.getConversationId());
+    if ((notifyConfig & FLAG_NOTIFY_MSGUPDATED) == FLAG_NOTIFY_MSGUPDATED) {
+      count[NOTIFY_INDEX_MSGUPDATED] ++;
+    }
   }
 
   /**
@@ -142,6 +242,9 @@ public class DummyConversationEventHandler extends AVIMConversationEventHandler 
    */
   public void onMessageRecalled(AVIMClient client, AVIMConversation conversation, AVIMMessage message) {
     LOGGER.d("Notification --- onMessageRecalled. client=" + client.getClientId() + ", convId=" + conversation.getConversationId());
+    if ((notifyConfig & FLAG_NOTIFY_MSGRECALLED) == FLAG_NOTIFY_MSGRECALLED) {
+      count[NOTIFY_INDEX_MSGRECALLED] ++;
+    }
   }
 
 }
