8d04ccc18764fe6b8df16d2bd778b691e1a81927
jwfing <jwfing@gmail.com>
2019-09-02 22:18:29 +0800

    add testcase for AVPush

--- core/src/test/java/cn/leancloud/AVPushTest.java
+++ core/src/test/java/cn/leancloud/AVPushTest.java
@@ -1,8 +1,17 @@
 package cn.leancloud;
 
+import com.alibaba.fastjson.JSONObject;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 import junit.framework.TestCase;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+
 public class AVPushTest extends TestCase {
+  private boolean testSucceed;
+
   public AVPushTest(String name) {
     super(name);
     Configure.initializeRuntime();
@@ -12,5 +21,40 @@ public class AVPushTest extends TestCase {
     AVPush push = new AVPush();
     push.setMessage("test from unittest");
     push.send();
+  }
+
+  public void testPushTargetWithData() throws Exception {
+    final CountDownLatch latch = new CountDownLatch(1);
+    testSucceed = false;
+    AVPush push = new AVPush();
+    Map<String, Object> pushData = new HashMap<>();
+    pushData.put("alert", "push message to android device directly");
+    push.setPushToAndroid(true);
+    push.setData(pushData);
+    push.sendInBackground().subscribe(new Observer<JSONObject>() {
+      @Override
+      public void onSubscribe(Disposable d) {
+      }
+
+      @Override
+      public void onNext(JSONObject jsonObject) {
+        System.out.println("推送成功" + jsonObject);
+        testSucceed = true;
+        latch.countDown();
+      }
+
+      @Override
+      public void onError(Throwable e) {
+        e.printStackTrace();
+        System.out.println("推送失败，错误信息：" + e.getMessage());
+        latch.countDown();
+      }
+
+      @Override
+      public void onComplete() {
+      }
+    });
+    latch.await();
+    assertTrue(testSucceed);
   }
 }
