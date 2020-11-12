6683674e11167b152570375003ff67c5b6dc8920
jwfing <jwfing@gmail.com>
2020-11-12 15:45:05 +0800

    version bump

--- core/src/main/java/cn/leancloud/core/AppConfiguration.java
+++ core/src/main/java/cn/leancloud/core/AppConfiguration.java
@@ -37,7 +37,7 @@ public class AppConfiguration {
 
   private static boolean enableLocalCache = true;
 
-  private static final String SDK_VERSION = "6.5.13";
+  private static final String SDK_VERSION = "6.5.14";
   private static final String DEFAULT_USER_AGENT = "LeanCloud-Java-SDK/" + SDK_VERSION;
 
   public static void setNetworkTimeout(int seconds) {
