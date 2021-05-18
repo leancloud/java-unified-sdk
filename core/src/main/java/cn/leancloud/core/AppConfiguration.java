acb75391f7d165991ea7bd7b8052b1da147a59a1
jwfing <jwfing@gmail.com>
2021-05-18 12:40:12 +0800

    version bump

--- core/src/main/java/cn/leancloud/core/AppConfiguration.java
+++ core/src/main/java/cn/leancloud/core/AppConfiguration.java
@@ -45,7 +45,7 @@ public class AppConfiguration {
   private static retrofit2.Converter.Factory retrofitConverterFactory = converterFactory.generateRetrofitConverterFactory();
   private static JSONParser jsonParser = converterFactory.createJSONParser();
 
-  private static final String SDK_VERSION = "7.2.6";
+  private static final String SDK_VERSION = "7.2.7";
   private static final String DEFAULT_USER_AGENT = "LeanCloud-Java-SDK/" + SDK_VERSION;
 
   public static void setNetworkTimeout(int seconds) {
