69ec8bf9809de674340cfc7ef49c753f29c14e82
jwfing <jwfing@gmail.com>
2021-07-07 19:08:53 +0800

    remove dependency of androidx in storage-android

--- android-sdk/storage-android/src/main/java/cn/leancloud/util/AndroidUtil.java
+++ android-sdk/storage-android/src/main/java/cn/leancloud/util/AndroidUtil.java
@@ -1,6 +1,9 @@
 package cn.leancloud.util;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Looper;
+import android.os.Process;
 
 /**
  * Created by fengjunwen on 2018/8/11.
@@ -9,5 +12,12 @@ import android.os.Looper;
 public class AndroidUtil {
   public static boolean isMainThread() {
     return (Looper.myLooper() == Looper.getMainLooper());
+  }
+
+  public static int checkPermission(Context context, String permission) {
+    if (null == context) {
+      return PackageManager.PERMISSION_DENIED;
+    }
+    return context.checkPermission(permission, android.os.Process.myPid(), Process.myUid());
   }
 }
