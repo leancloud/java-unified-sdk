9234e7ef5733238017d23241996089b77564805a
jwfing <jwfing@gmail.com>
2021-07-08 08:17:47 +0800

    add license description

--- android-sdk/realtime-android/src/main/java/cn/leancloud/utils/RemoteInputCompatBase.java
+++ android-sdk/realtime-android/src/main/java/cn/leancloud/utils/RemoteInputCompatBase.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package cn.leancloud.utils;
 
 import android.os.Bundle;
@@ -15,9 +31,9 @@ class RemoteInputCompatBase {
         protected abstract boolean getAllowFreeFormInput();
         protected abstract Bundle getExtras();
         public interface Factory {
-            public RemoteInput build(String resultKey, CharSequence label,
+            RemoteInput build(String resultKey, CharSequence label,
                                      CharSequence[] choices, boolean allowFreeFormInput, Bundle extras);
-            public RemoteInput[] newArray(int length);
+            RemoteInput[] newArray(int length);
         }
     }
 }
